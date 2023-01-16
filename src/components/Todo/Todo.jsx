686ced581bdb4ac71ac82ca9658f90ec3a2ee36a
tmdgus95 <8571053@naver.com>
2023-01-16 23:18:42 +0900

    인풋 아이디 버그 수정

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,7 +3,7 @@ import { FaTrashAlt } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 export default function Todo({ todo, onUpdate, onDelete }) {
-    const { status, text } = todo;
+    const { id, status, text } = todo;
     const handleDelete = () => onDelete(todo);
     const handleChange = (e) => {
         let status = e.target.checked ? 'completed' : 'active';
@@ -14,11 +14,11 @@ export default function Todo({ todo, onUpdate, onDelete }) {
             <input
                 className={styles.checkbox}
                 type='checkbox'
-                id='checkbox'
+                id={id}
                 checked={status === 'completed'}
                 onChange={handleChange}
             />
-            <label htmlFor='checkbox' className={styles.text}>
+            <label htmlFor={id} className={styles.text}>
                 {text}
             </label>
             <span className={styles.icon}>
