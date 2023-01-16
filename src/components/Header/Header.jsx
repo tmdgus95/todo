f0c1e3cc7a15c2bc9d4ad6cbd0e9298e5fcb525d
tmdgus95 <8571053@naver.com>
2023-01-16 21:01:16 +0900

    다크 모드 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { HiSun, HiMoon } from 'react-icons/hi';
+import { useDarkMode } from '../../context/DarkModeContext';
 
 export default function Header({ filters, filter, onFilterChange }) {
+    const { darkMode, toggleDarkMode } = useDarkMode();
     return (
         <header className={styles.header}>
+            <button onClick={toggleDarkMode}>
+                {darkMode && <HiSun />}
+                {!darkMode && <HiMoon />}
+            </button>
             <ul className={styles.filters}>
                 {filters.map((value, index) => (
                     <li key={index}>
